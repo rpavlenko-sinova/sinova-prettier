63f60f9178dbb73913b711a94c11d9a4b60c6994
rpavlenko-sinova <rpavlenko.sinova@gmail.com>
2025-07-15 15:05:56 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const config = {
+  printWidth: 120,
+  trailingComma: "all",
+  singleQuote: true,
+  singleAttributePerLine: true,
+  singleQuote: true,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
